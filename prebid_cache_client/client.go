1ca6a7294d3598c03f7e09bf0900efd9715efe14
Qili <qili@Qilis-MacBook-Pro.local>
2020-07-18 12:03:14 -0700

    Revert "log prebid cache response for debug"
    This reverts commit 5d1b39e4cb6cca48bfea15f8c5be3fbad4a1617a.

--- prebid_cache_client/client.go
+++ prebid_cache_client/client.go
@@ -119,8 +119,6 @@ func (c *clientImpl) PutJson(ctx context.Context, values []Cacheable) (uuids []s
 	c.metrics.RecordPrebidCacheRequestTime(true, elapsedTime)
 
 	responseBody, err := ioutil.ReadAll(anResp.Body)
-	//
-	logError(&errs, "Prebid Cache call to %s returned %d: %s", putURL, anResp.StatusCode, responseBody)
 	if anResp.StatusCode != 200 {
 		logError(&errs, "Prebid Cache call to %s returned %d: %s", c.putUrl, anResp.StatusCode, responseBody)
 		return uuidsToReturn, errs
