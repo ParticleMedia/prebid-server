7f3dd46dc9425aef0ba2247bc65df24d0d5d9994
Brian Sardo <1168933+bsardo@users.noreply.github.com>
2022-09-13 21:50:48 -0400

    GDPR: add enforce_algo config flag and convert enforce_purpose to bool (#2330)

--- endpoints/openrtb2/auction.go
+++ endpoints/openrtb2/auction.go
@@ -1875,6 +1875,10 @@ func writeError(errs []error, w http.ResponseWriter, labels *metrics.Labels) boo
 				httpStatus = http.StatusServiceUnavailable
 				metricsStatus = metrics.RequestStatusBlacklisted
 				break
+			} else if erVal == errortypes.MalformedAcctErrorCode {
+				httpStatus = http.StatusInternalServerError
+				metricsStatus = metrics.RequestStatusAccountConfigErr
+				break
 			}
 		}
 		w.WriteHeader(httpStatus)
