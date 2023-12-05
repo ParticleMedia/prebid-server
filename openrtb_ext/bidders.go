aed2f6ab90d86f53a7fd2fb8fcc5a50160aff2ce
Hugo Lindström <hugolm84@gmail.com>
2023-12-05 15:50:43 +0100

    New Adapter: Relevant Digital (#3216)
    co-authored by: @hugolm84

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -158,6 +158,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderPubnative,
 	BidderPulsepoint,
 	BidderPWBid,
+	BidderRelevantDigital,
 	BidderRevcontent,
 	BidderRichaudience,
 	BidderRise,
@@ -438,6 +439,7 @@ const (
 	BidderPubnative         BidderName = "pubnative"
 	BidderPulsepoint        BidderName = "pulsepoint"
 	BidderPWBid             BidderName = "pwbid"
+	BidderRelevantDigital   BidderName = "relevantdigital"
 	BidderRevcontent        BidderName = "revcontent"
 	BidderRichaudience      BidderName = "richaudience"
 	BidderRise              BidderName = "rise"
