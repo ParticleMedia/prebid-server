2a21fae4d83e073992596e6fd7169a6de4bea768
readpeak-user <63640438+readpeak-user@users.noreply.github.com>
2024-04-25 12:08:10 +0300

    New Adapter: Readpeak (#3610)
    Co-authored-by: Tuomo Tilli <tuomo.tilli@readpeak.com>

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -163,6 +163,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderPubnative,
 	BidderPulsepoint,
 	BidderPWBid,
+	BidderReadpeak,
 	BidderRelevantDigital,
 	BidderRevcontent,
 	BidderRichaudience,
@@ -450,6 +451,7 @@ const (
 	BidderPubnative         BidderName = "pubnative"
 	BidderPulsepoint        BidderName = "pulsepoint"
 	BidderPWBid             BidderName = "pwbid"
+	BidderReadpeak          BidderName = "readpeak"
 	BidderRelevantDigital   BidderName = "relevantdigital"
 	BidderRevcontent        BidderName = "revcontent"
 	BidderRichaudience      BidderName = "richaudience"
