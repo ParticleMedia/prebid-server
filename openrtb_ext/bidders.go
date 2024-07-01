da92d22682448a2648d3ee95cf2c2d07921f6c71
Saar Amrani <saar120@gmail.com>
2024-07-01 10:54:47 +0300

    New Adapter: Vidazoo (#3698)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -208,6 +208,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderUndertone,
 	BidderUnicorn,
 	BidderUnruly,
+	BidderVidazoo,
 	BidderVideoByte,
 	BidderVideoHeroes,
 	BidderVidoomy,
@@ -524,6 +525,7 @@ const (
 	BidderUndertone         BidderName = "undertone"
 	BidderUnicorn           BidderName = "unicorn"
 	BidderUnruly            BidderName = "unruly"
+	BidderVidazoo           BidderName = "vidazoo"
 	BidderVideoByte         BidderName = "videobyte"
 	BidderVideoHeroes       BidderName = "videoheroes"
 	BidderVidoomy           BidderName = "vidoomy"
