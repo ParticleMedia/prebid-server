953d3bd060fe226df3a811081caba828579ee7e6
Viral Vala <63396712+pm-viral-vala@users.noreply.github.com>
2024-12-16 19:39:06 +0530

    PubMatic: Remove req.ext.prebid from bid requests (#4092)

--- adapters/pubmatic/pubmatic.go
+++ adapters/pubmatic/pubmatic.go
@@ -71,7 +71,6 @@ type extRequestAdServer struct {
 	Wrapper     *pubmaticWrapperExt `json:"wrapper,omitempty"`
 	Acat        []string            `json:"acat,omitempty"`
 	Marketplace *marketplaceReqExt  `json:"marketplace,omitempty"`
-	openrtb_ext.ExtRequest
 }
 
 type respExt struct {
@@ -371,7 +370,6 @@ func extractPubmaticExtFromRequest(request *openrtb2.BidRequest) (extRequestAdSe
 	if err != nil {
 		return pmReqExt, fmt.Errorf("error decoding Request.ext : %s", err.Error())
 	}
-	pmReqExt.ExtRequest = *reqExt
 
 	reqExtBidderParams := make(map[string]json.RawMessage)
 	if reqExt.Prebid.BidderParams != nil {
