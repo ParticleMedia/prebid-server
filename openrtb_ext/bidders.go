f45e41cd4495bfe709df7e21de80d03a184845d5
Leandro Marty <leandro.marty@rocketlab.ai>
2025-02-11 17:54:25 +0100

    New Adapter: Smoot (#4148)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -214,6 +214,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderSmartx,
 	BidderSmartyAds,
 	BidderSmileWanted,
+	BidderSmoot,
 	BidderSmrtconnect,
 	BidderSonobi,
 	BidderSovrn,
@@ -565,6 +566,7 @@ const (
 	BidderSmartx            BidderName = "smartx"
 	BidderSmartyAds         BidderName = "smartyads"
 	BidderSmileWanted       BidderName = "smilewanted"
+	BidderSmoot             BidderName = "smoot"
 	BidderSmrtconnect       BidderName = "smrtconnect"
 	BidderSonobi            BidderName = "sonobi"
 	BidderSovrn             BidderName = "sovrn"
