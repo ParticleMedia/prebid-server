0d637d03d94faf35bc4c26e1ff861fed41dfb2d7
haoyangyuan <haoyang.yuan@newsbreak.com>
2025-02-21 13:31:43 -0800

    implement db+file for prebid-server

--- endpoints/openrtb2/auction.go
+++ endpoints/openrtb2/auction.go
@@ -2029,7 +2029,54 @@ func getJsonSyntaxError(testJSON []byte) (bool, string) {
 	return false, ""
 }
 
+func isDbFetched(requestJson []byte) bool {
+	val, dataType, _, err := jsonparser.Get(requestJson, "ext", "db_fetched")
+	if err != nil {
+		return false
+	}
+	if dataType == jsonparser.Boolean && string(val) == "true" {
+		return true
+	}
+	return false
+}
+
+func parseDbStoredMaps(requestJson []byte) (map[string]json.RawMessage, map[string]json.RawMessage, []error) {
+	var errs []error
+	storedReqMap := make(map[string]json.RawMessage)
+	storedImpMap := make(map[string]json.RawMessage)
+
+	rawReqs, dt, _, err := jsonparser.Get(requestJson, "ext", "db_storedrequests")
+	if err == nil && dt == jsonparser.Object {
+		if e := json.Unmarshal(rawReqs, &storedReqMap); e != nil {
+			errs = append(errs, fmt.Errorf("failed to unmarshal db_storedrequests: %v", e))
+		}
+	}
+
+	rawImps, dt2, _, err2 := jsonparser.Get(requestJson, "ext", "db_storedimps")
+	if err2 == nil && dt2 == jsonparser.Object {
+		if e := json.Unmarshal(rawImps, &storedImpMap); e != nil {
+			errs = append(errs, fmt.Errorf("failed to unmarshal db_storedimps: %v", e))
+		}
+	}
+
+	return storedReqMap, storedImpMap, errs
+}
+
 func (deps *endpointDeps) getStoredRequests(ctx context.Context, requestJson []byte, impInfo []ImpExtPrebidData) (string, bool, map[string]json.RawMessage, map[string]json.RawMessage, []error) {
+	if isDbFetched(requestJson) {
+		dbReqMap, dbImpMap, parseErrs := parseDbStoredMaps(requestJson)
+		if len(parseErrs) > 0 {
+			return "", false, nil, nil, parseErrs
+		}
+
+		// same parameters as normal
+		storedBidRequestId, hasStoredBidRequest, err := getStoredRequestId(requestJson)
+		if err != nil {
+			return "", false, nil, nil, []error{err}
+		}
+
+		return storedBidRequestId, hasStoredBidRequest, dbReqMap, dbImpMap, nil
+	}
 	// Parse the Stored Request IDs from the BidRequest and Imps.
 	storedBidRequestId, hasStoredBidRequest, err := getStoredRequestId(requestJson)
 	if err != nil {
