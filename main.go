6ce68e8220a67a0fea4f99c019c45d329ab9f462
shunj-nb <75647253+shunj-nb@users.noreply.github.com>
2025-03-05 22:13:04 -0800

    mspai-config (#77)

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"time"
 
 	jsoniter "github.com/json-iterator/go"
@@ -60,7 +62,17 @@ const infoDirectory = "./static/bidder-info"
 
 func loadConfig(bidderInfos config.BidderInfos) (*config.Configuration, error) {
 	v := viper.New()
-	config.SetupViper(v, configFileName, bidderInfos)
+	mspConfigFileName := configFileName
+	envVar := strings.ToLower(os.Getenv("MSP_RUN_MODE"))
+
+	switch envVar {
+	case "mspai_staging":
+		mspConfigFileName = "./msp_config/mspai-stage"
+	case "mspai_prod":
+		mspConfigFileName = "./msp_config/mspai-prod"
+	}
+
+	config.SetupViper(v, mspConfigFileName, bidderInfos)
 	return config.New(v, bidderInfos, openrtb_ext.NormalizeBidderName)
 }
 
