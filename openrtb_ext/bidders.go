bd9828d8cee5ffcf77047019a51fe7cd323e628d
prebid-startio <prebid@start.io>
2025-06-05 06:21:51 +0300

    New Adapter: Start.io (#4324)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -227,6 +227,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderSovrn,
 	BidderSovrnXsp,
 	BidderSspBC,
+	BidderStartIO,
 	BidderStroeerCore,
 	BidderTaboola,
 	BidderTappx,
@@ -586,6 +587,7 @@ const (
 	BidderSovrn             BidderName = "sovrn"
 	BidderSovrnXsp          BidderName = "sovrnXsp"
 	BidderSspBC             BidderName = "sspBC"
+	BidderStartIO           BidderName = "startio"
 	BidderStroeerCore       BidderName = "stroeerCore"
 	BidderTaboola           BidderName = "taboola"
 	BidderTappx             BidderName = "tappx"
