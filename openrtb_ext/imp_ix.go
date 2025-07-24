411e77ed357d4446430be8b6b99e98f1c11d3cff
shun jiang <shun.jiang@newsbreak.com>
2025-07-24 14:41:51 -0700

    support ix imp level endpoint

--- openrtb_ext/imp_ix.go
+++ openrtb_ext/imp_ix.go
@@ -2,7 +2,8 @@ package openrtb_ext
 
 // ExtImpIx defines the contract for bidrequest.imp[i].ext.prebid.bidder.ix
 type ExtImpIx struct {
-	SiteId string `json:"siteId"`
-	Size   []int  `json:"size"`
-	Sid    string `json:"sid"`
+	SiteId   string `json:"siteId"`
+	Size     []int  `json:"size"`
+	Sid      string `json:"sid"`
+	Endpoint string `json:"ix_endpoint"`
 }
