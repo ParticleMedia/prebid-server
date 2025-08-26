236a1b55c7c9d270c9c46768da47a4d4168c3a51
zwl-williamzha <zwlwilliamzha@gmail.com>
2025-08-26 13:53:51 -0700

    add field

--- openrtb_ext/imp_vidazoo.go
+++ openrtb_ext/imp_vidazoo.go
@@ -3,4 +3,5 @@ package openrtb_ext
 // ImpExtVidazoo defines the contract for bidrequest.imp[i].ext.prebid.bidder.vidazoo
 type ImpExtVidazoo struct {
 	ConnectionId string `json:"cId"`
+	TagId        string `json:"tagId"`
 }
