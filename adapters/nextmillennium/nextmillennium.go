db542ede8ffe3415340102d7ec020b5d49a66699
zwl-williamzha <zwlwilliamzha@gmail.com>
2025-11-24 13:35:36 -0800

    Refactor

--- adapters/nextmillennium/nextmillennium.go
+++ adapters/nextmillennium/nextmillennium.go
@@ -47,10 +47,6 @@ type nextMillJsonExt struct {
 
 // MakeRequests prepares request information for prebid-server core
 func (adapter *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	fmt.Printf("Address of NM imp[0] = %p\n", &request.Imp[0])
-	fmt.Printf("Address of NM app = %p\n", &request.App)
-	fmt.Printf("Address of NM device = %p\n", &request.Device)
-	fmt.Printf("Address of NM user = %p\n", &request.User)
 
 	resImps, err := getImpressionsInfo(request.Imp)
 	if len(err) > 0 {
