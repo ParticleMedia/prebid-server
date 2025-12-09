24713f5f6aa5754235e204a9696f07adb0fc910e
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-12-09 15:21:04 +0100

    Logging: Add interface with default glog implementation (#4085)
    Co-authored-by: postindustria-code <oss@postindustria.com>

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ import (
 	jsoniter "github.com/json-iterator/go"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/currency"
+	"github.com/prebid/prebid-server/v3/logger"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
 	"github.com/prebid/prebid-server/v3/router"
 	"github.com/prebid/prebid-server/v3/server"
 	"github.com/prebid/prebid-server/v3/util/jsonutil"
 	"github.com/prebid/prebid-server/v3/util/task"
 
-	"github.com/golang/glog"
 	"github.com/spf13/viper"
 )
 
@@ -31,16 +31,17 @@ func main() {
 
 	bidderInfoPath, err := filepath.Abs(infoDirectory)
 	if err != nil {
-		glog.Exitf("Unable to build configuration directory path: %v", err)
+		logger.Fatalf("Unable to build configuration directory path: %v", err)
 	}
 
 	bidderInfos, err := config.LoadBidderInfoFromDisk(bidderInfoPath)
 	if err != nil {
-		glog.Exitf("Unable to load bidder configurations: %v", err)
+		logger.Fatalf("Unable to load bidder configurations: %v", err)
 	}
+
 	cfg, err := loadConfig(bidderInfos)
 	if err != nil {
-		glog.Exitf("Configuration could not be loaded or did not pass validation: %v", err)
+		logger.Fatalf("Configuration could not be loaded or did not pass validation: %v", err)
 	}
 
 	// Create a soft memory limit on the total amount of memory that PBS uses to tune the behavior
@@ -53,7 +54,7 @@ func main() {
 
 	err = serve(cfg)
 	if err != nil {
-		glog.Exitf("prebid-server failed: %v", err)
+		logger.Fatalf("prebid-server failed: %v", err)
 	}
 }
 
@@ -96,7 +97,7 @@ func serve(cfg *config.Configuration) error {
 
 	corsRouter := router.SupportCORS(r)
 	if err := server.Listen(cfg, router.NoCache{Handler: corsRouter}, router.Admin(currencyConverter, fetchingInterval), r.MetricsEngine); err != nil {
-		glog.Fatalf("prebid-server returned an error: %v", err)
+		logger.Fatalf("prebid-server returned an error: %v", err)
 	}
 
 	r.Shutdown()
