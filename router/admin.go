709723b9295f760677736e06956db530066cccc4
zwl-williamzha <zwlwilliamzha@gmail.com>
2026-01-12 15:55:15 -0800

    Go delta prof

--- router/admin.go
+++ router/admin.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/grafana/pyroscope-go/godeltaprof"
 	"net/http"
 	"net/http/pprof"
 	"time"
@@ -14,12 +15,21 @@ func Admin(rateConverter *currency.RateConverter, rateConverterFetchingInterval 
 	// Add endpoints to the admin server
 	// Making sure to add pprof routes
 	mux := http.NewServeMux()
+
 	// Register pprof handlers
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	// NewsBreak Custom Code: delta_heap (Go 1.20+ only)
+	dhp := godeltaprof.NewHeapProfiler()
+	mux.HandleFunc("/debug/pprof/delta_heap", func(w http.ResponseWriter, r *http.Request) {
+		// This calculates the delta since the last time this endpoint was called
+		dhp.Profile(w)
+	})
+
 	// Register prebid-server defined admin handlers
 	mux.HandleFunc("/currency/rates", endpoints.NewCurrencyRatesEndpoint(rateConverter, rateConverterFetchingInterval))
 	mux.HandleFunc("/version", endpoints.NewVersionEndpoint(version.Ver, version.Rev))
